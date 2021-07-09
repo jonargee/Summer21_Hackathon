624d909a8721b1e8d43a92da3d1fca218f3f6f81
jonargee <rammgraj@oregonstate.edu>
2021-07-09 17:47:08 -0600

    fat cat moves!

--- main.py
+++ main.py
@@ -13,21 +13,41 @@ CAT_WIDTH, CAT_HEIGHT = 100, 150
 CAT_IMAGE = pygame.image.load(os.path.join("assets", "cat.png"))
 CAT = pygame.transform.scale(CAT_IMAGE, (CAT_WIDTH, CAT_HEIGHT))
 
-def draw_window():
+VEL = 5
+FPS = 60
+
+
+def draw_window(cat):
     WIN.fill(WHITE)
-    WIN.blit(CAT, (50, 50))
+    WIN.blit(CAT, (cat.x, cat.y))
     pygame.display.update()
 
 
-def main():
+def handle_cat_movement(keys_pressed, cat):
+    if keys_pressed[pygame.K_a] and cat.x - VEL > 0:  # left
+        cat.x -= VEL
+    if keys_pressed[pygame.K_d] and cat.x + VEL < WIDTH - CAT_WIDTH:  # right
+        cat.x += VEL
+    if keys_pressed[pygame.K_w] and cat.y - VEL > 0:  # up
+        cat.y -= VEL
+    if keys_pressed[pygame.K_s] and cat.y + VEL < cat.height - CAT_HEIGHT:  # down
+        cat.y += VEL
 
+
+def main():
+    cat = pygame.Rect(100, 300, WIDTH, HEIGHT)
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        keys_pressed = pygame.key.get_pressed()
+        handle_cat_movement(keys_pressed, cat)
+        draw_window(cat)
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
