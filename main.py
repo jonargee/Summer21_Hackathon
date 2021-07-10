136dad908a49bad1f1175c98358c72c4a57503d0
Genevieve Will <64991919+genefwill@users.noreply.github.com>
2021-07-09 18:13:14 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,137 @@
-import pygame
-import os
-from pygame import mixer
-mixer.init()
+import pygame, sys
 
-WIDTH, HEIGHT = 1200, 700
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Cat Will One Day Eat Me")
+clock = pygame.time.Clock()
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0, 0)
+from pygame.locals import *
+pygame.init()
 
-CAT_WIDTH, CAT_HEIGHT = 100, 150
+pygame.display.set_caption('Pygame Window')
 
-CAT_IMAGE = pygame.image.load(os.path.join("assets", "cat.png"))
-CAT = pygame.transform.scale(CAT_IMAGE, (CAT_WIDTH, CAT_HEIGHT))
+WINDOW_SIZE = (600,400)
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
+display = pygame.Surface((600,400))
 
-background_music = mixer.music.load(os.path.join("assets", "toaf.mp3"))
-mixer.music.play(-1)
-
-VEL = 5
-FPS = 60
+player_image = pygame.image.load('cat.png')
+floor_image = pygame.image.load('check.png')
+FLOOR_TILE = floor_image.get_width()
+shelf_image = pygame.image.load('shelf.png')
+SHELF_TILE = shelf_image.get_width()
 
 
-def draw_window(cat):
-    WIN.fill(WHITE)
-    WIN.blit(CAT, (cat.x, cat.y))
+
+game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','2','2','2','2','2','2','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
+
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+def move(rect,movement,tiles):
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right']= True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[0] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+    return rect, collision_types
+
+moving_right = False
+moving_left = False
+
+player_location = [50,300]
+player_y_momentum = 0
+
+player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
+test_rect = pygame.Rect(100, 100, 100,50)
+while True:
+    display.fill((146,244,255))
+
+    tile_rects = []
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == '1':
+                display.blit(floor_image, (x * FLOOR_TILE, y * FLOOR_TILE))
+            if tile == '2':
+                display.blit(shelf_image, (x * SHELF_TILE, y * SHELF_TILE))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(x * SHELF_TILE, y * SHELF_TILE, SHELF_TILE, SHELF_TILE))
+
+            x += 1
+
+        y += 1
+
+
+
+
+
+    player_movement = [0,0]
+    if moving_right:
+        player_movement[0] += 2
+    if moving_left:
+        player_movement[0] -= 2
+    player_movement[1] += player_y_momentum
+    player_y_momentum += 0.2
+    if player_y_momentum > 3:
+        player_y_momentum = 3
+
+
+    player_rect, collisions = move(player_rect, player_movement, tile_rects)
+
+    display.blit(player_image, (player_rect.x, player_rect.y))
+
+    if player_rect.colliderect(test_rect):
+        pygame.draw.rect(screen,(255,0,0),test_rect)
+    else:
+        pygame.draw.rect(screen, (0, 0, 0), test_rect)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                moving_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+            if event.key == K_UP:
+                player_y_momentum = -6
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
+
+    surf = pygame.transform.scale(display, WINDOW_SIZE)
+    screen.blit(surf,(0,0))
     pygame.display.update()
-
-
-def handle_cat_movement(keys_pressed, cat):
-    if keys_pressed[pygame.K_a] and cat.x - VEL > 0:  # left
-        cat.x -= VEL
-    if keys_pressed[pygame.K_d] and cat.x + VEL < WIDTH - CAT_WIDTH:  # right
-        cat.x += VEL
-    if keys_pressed[pygame.K_w] and cat.y - VEL > 0:  # up
-        cat.y -= VEL
-    if keys_pressed[pygame.K_s] and cat.y + VEL < cat.height - CAT_HEIGHT:  # down
-        cat.y += VEL
-
-
-def main():
-    cat = pygame.Rect(100, 300, WIDTH, HEIGHT)
-    clock = pygame.time.Clock()
-
-    run = True
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        keys_pressed = pygame.key.get_pressed()
-        handle_cat_movement(keys_pressed, cat)
-        draw_window(cat)
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+    clock.tick(60)
