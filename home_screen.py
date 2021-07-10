bb28894489c5e089db69b7f3f726c7a8557eec27
jonargee <81392163+jonargee@users.noreply.github.com>
2021-07-10 15:38:12 -0600

    started home_screen.py

--- home_screen.py
+++ home_screen.py
@@ -1,6 +1,8 @@
 import pygame
 import pygame.freetype
 from pygame.sprite import Sprite
+from music import play_title_scr_music
+from enum import Enum
 from pygame.rect import Rect
 
 GREEN = (78, 245, 56)
@@ -14,11 +16,17 @@ def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
     return surface.convert_alpha()
 
 
+class GameState(Enum):
+    QUIT = -1
+    TITLE = 0
+    START = 1
+
+
 class UIElement(Sprite):
     """UI for menu buttons"""
 
 
-    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb):
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb, action=None):
         self.mouse_over = False
         super().__init__()
         default_image = create_surface_with_text(text, font_size, text_rgb, bg_rgb)
@@ -27,6 +35,7 @@ class UIElement(Sprite):
         self.rects = [
             default_image.get_rect(center=center_position),
             highlighted_image.get_rect(center=center_position)]
+        self.action = action
 
     @property
     def image(self):
@@ -36,35 +45,57 @@ class UIElement(Sprite):
     def rect(self):
         return self.rects[1] if self.mouse_over else self.rects[0]
 
-    def update(self, mouse_pos):
+    def update(self, mouse_pos, mouse_up):
+        """updates the button appearance and returns button's action if clicked"""
         if self.rect.collidepoint(mouse_pos):
             self.mouse_over = True
+            if mouse_up:
+                return self.action
         else:
             self.mouse_over = False
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
+    play_title_scr_music()
+
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((800, 600))
-    uielement = UIElement(
-        center_position=(400, 300),
+    # uielement = UIElement(
+    #     center_position=(400, 300),
+    #     font_size=30,
+    #     bg_rgb=GREEN,
+    #     text_rgb=BLACK,
+    #     text="Welcome to the Apo-cat-lypse"
+    # )
+
+    quit_uielement = UIElement(
+        center_position=(400, 200),
         font_size=30,
         bg_rgb=GREEN,
         text_rgb=BLACK,
-        text="Welcome to the Apo-cat-lypse"
+        text="QUIT",
+        action=GameState.QUIT
     )
 
     run = True
     while run is True:
+        mouse_up = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
             screen.fill(GREEN)
-            uielement.update(pygame.mouse.get_pos())
-            uielement.draw(screen)
+            ui_action = quit_uielement.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return
+            quit_uielement.update(pygame.mouse.get_pos(), mouse_up)
+            quit_uielement.draw(screen)
+            # uielement.update(pygame.mouse.get_pos())
+            # uielement.draw(screen)
             pygame.display.flip()
 
     pygame.quit()
--- music.py
+++ music.py
@@ -3,7 +3,14 @@ from pygame import mixer
 pygame.init()
 mixer.init()
 
-def play_lv1_music():
+
+def play_level_music():
     """plays level one music"""
     mixer.music.load("toaf_remix.mp3")
+    mixer.music.play(-1)
+
+
+def play_title_scr_music():
+    """plays title screen music"""
+    mixer.music.load("toaf_menu_screen.mp3")
     mixer.music.play(-1)
