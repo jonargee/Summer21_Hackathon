3727f7d30046403e050a59482e962e524a705304
Genevieve Will <64991919+genefwill@users.noreply.github.com>
2021-07-10 17:35:40 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+import random
 
 clock = pygame.time.Clock()
 
@@ -13,9 +14,7 @@ screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
 display = pygame.Surface((600,400))
 
 #Images for game
-cat_image = pygame.image.load('cat.png')
-left_player_image = pygame.transform.flip(cat_image, True, False)
-right_player_image = pygame.image.load('cat.png')
+player_image = pygame.image.load('cat.png')
 floor_image = pygame.image.load('check.png')
 FLOOR_TILE = floor_image.get_width()
 shelf_image = pygame.image.load('shelf.png')
@@ -26,12 +25,71 @@ black_image = pygame.image.load('black.png')
 BLACK_TILE = shelf_image.get_width()
 grey_image = pygame.image.load('grey.png')
 GREY_TILE = shelf_image.get_width()
+roach_image = pygame.image.load('roach.png')
+fish_img = pygame.image.load('fishbone.png')
+
 
 #creating scroll variable
 scroll = [0,0]
 
-#initializing directional orientation
-direction = 1
+class collectable():
+    def __init__(self, loc):
+        self.location = player_location
+
+    def render(self, surf, scroll):
+        display.blit(fish_img, (self.loc[0] - scroll[0], self.loc[1] - scroll[1]))
+
+    def get_rect(self):
+        return pygame.Rect(self.loc[0], self.loc[1], 32, 32)
+
+    def collect(self):
+        collect_rect = self.get_rect()
+        return collect_rect.colliderect(rect)
+
+
+
+
+class enemy(object):
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.end]
+        self.walk_count = 0
+        self.vel = 2
+        self.roach_img = pygame.image.load('roach.png')
+        self.hitbox = (self.x, self.y, 25, 30)
+
+    def draw(self,win):
+        self.move()
+        if self.walk_count +1 <= 33:
+            self.walk_count = 0
+        if self.vel > 0:
+            display.blit(self.roach_img, (self.x - scroll[0], self.y - scroll[1]))
+            self.walk_count += 1
+        else:
+            display.blit(self.roach_img, (self.x - scroll[0], self.y - scroll[1]))
+            self.walk_count += 1
+
+    def hit(self):
+        print('hit')
+
+    def move(self):
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walk_count = 0
+        else:
+            if self.x - self.vel > self.path[0]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walk_count = 0
 
 def load_map(path):
     """
@@ -47,8 +105,25 @@ def load_map(path):
     for row in data:
         game_map.append(list(row))
     return game_map
-
 game_map = load_map('map')
+
+
+#old game map that I didn't want to erase because I spent a lot of time writing it
+game_map2 = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','2','2','2','2','2','2','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
+
+
 
 def collision_test(rect, tiles):
     """
@@ -99,7 +174,19 @@ player_y_momentum = 0
 air_timer = 0
 
 #Defining player rectangle
-player_rect = pygame.Rect(player_location[0],player_location[1],cat_image.get_width(),cat_image.get_height())
+player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
+
+
+enemy_location = [700,285]
+enemy_rect = pygame.Rect(enemy_location[0], enemy_location[1], roach_image.get_width(), roach_image.get_height())
+roach1 = enemy(200, 295, 16, 16, 230)
+roach2 = enemy(450, 295, 16, 16, 480)
+
+score = 0
+collect_objects = []
+
+for i in range(5):
+    collect_objects.append(collectable((random.randint(0, 600) - 300, 80)))
 
 #Game loop
 while True:
@@ -134,15 +221,16 @@ while True:
 
         y += 1
 
+
+
+
     #defining character movement variable
     player_movement = [0,0]
     #Establing how player moves based on moving variable
     if moving_right:
         player_movement[0] += 2
-        direction = 1
     if moving_left:
         player_movement[0] -= 2
-        direction = 0
     player_movement[1] += player_y_momentum
     player_y_momentum += 0.2
     if player_y_momentum > 3:
@@ -150,7 +238,7 @@ while True:
 
 
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
-    # Checking if character collides with bottom or top of platform and establishes a jump time
+    #Checking if character collides with bottom or top of platform and establishes a jump time
     if collisions['bottom']:
         player_y_momentum = 0
         air_timer = 0
@@ -161,14 +249,28 @@ while True:
         player_y_momentum = 0
 
     #displaying of player
-    if direction == 1:
-        display.blit(right_player_image, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
-    elif direction == 0:
-        display.blit(left_player_image, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+    display.blit(player_image, (player_rect.x - scroll[0], player_rect.y- scroll[1]))
+
+    roach1.draw(display)
+    roach2.draw(display)
+
+    if player_rect.y < roach1.hitbox[1] + roach1.hitbox[3] and player_rect.y > roach1.hitbox[1]:
+        if player_rect.x > roach1.hitbox[0] and player_rect.x < roach1.hitbox[0] + roach1.hitbox[2]:
+            roach1.hit()
+
+
+
+
+    #for collectable in collect_objects:
+    #    collectable.render(display.scroll)
+    #    if collectables.collision_test(player_rect):
+     #       scrore += 1
+
 
     #Establishes movement by keystroke and Quiting of game loop
     for event in pygame.event.get():
         if event.type == QUIT:
+
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
