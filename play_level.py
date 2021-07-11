008820db91010085c38201b020f3c957f4e70961
jonargee <81392163+jonargee@users.noreply.github.com>
2021-07-11 12:08:56 -0600

    add collectable
    trying to add a fly collectable

--- play_level.py
+++ play_level.py
@@ -49,11 +49,17 @@ def play_level(screen):
         def __init__(self, x, y):
             self.x = x
             self.y = y
+            # self.col_type = col_type
             self.fish_img = pygame.image.load('fishbone.png')
+            self.fly_img = pygame.image.load("fly_2.png")
             self.hitbox = (self.x, self.y, 25, 30)
 
-        def draw(self, win):
-            display.blit(self.fish_img, (self.x - scroll[0], self.y - scroll[1]))
+        def draw(self, col_type, win):
+            if col_type == 1:
+                display.blit(self.fish_img, (self.x - scroll[0], self.y - scroll[1]))
+            elif col_type == 2:
+                display.blit(self.fly_img, (self.x - scroll[0], self.y - scroll[1]))
+
 
         def get_rect(self):
             return pygame.Rect(x, y, 32, 32)
@@ -205,6 +211,7 @@ def play_level(screen):
     fish3 = collectable(1155, 190)
     fish4 = collectable(1970, 64)
     fish5 = collectable(2398, 128)
+    fly1 = collectable(1056, 288)
 
     score = 0
 
@@ -290,11 +297,12 @@ def play_level(screen):
         roach4.draw(display)
         roach5.draw(display)
         roach6.draw(display)
-        fish1.draw(display)
-        fish2.draw(display)
-        fish3.draw(display)
-        fish4.draw(display)
-        fish5.draw(display)
+        fish1.draw(1, display)
+        fish2.draw(1, display)
+        fish3.draw(1, display)
+        fish4.draw(1, display)
+        fish5.draw(1, display)
+        fly1.draw(2, display)
         print(player_rect.x, player_rect.y)
 
 
